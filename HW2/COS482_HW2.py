873ed2b63bb0e81843adf7b020369abe65c8f18b
Kaleb Hannan <kaleb.hannan@maine.edu>
2024-11-12 00:04:08 -0500

    added data to the tables

--- HW2/COS482_HW2.py
+++ HW2/COS482_HW2.py
@@ -1,5 +1,5 @@
 import psycopg2
-from tqdm import tqdm
+from tqdm import tqdm 
 
 # Kaleb Hannan
 # HW2 SQL
@@ -28,7 +28,7 @@ cur.execute('''
             id integer NOT NULL PRIMARY KEY,
             name text,
             year integer,
-            rank REAL
+            rank NUMERIC
         );
 ''')
 conn.commit()
@@ -69,15 +69,13 @@ cur.execute('''
 conn.commit()
 
 # Add Data to tables
+'''
+# Add data to person table
 with open('HW2/IMDB/IMDBPerson.txt', 'r',  encoding='latin-1') as file:
-    total_lines = sum(1 for _ in file)
-    total_lines =- 1
-    file.seek(0)
     #skip first line with file lables
     file.readline()
     
-
-    for line in tqdm(file, total=total_lines, desc="Inserting Into Person", unit="row"):
+    for line in tqdm(file, total= 817718):
         splitString = line.strip().split(',')
         ID = splitString[0]
         fName = splitString[1]
@@ -86,14 +84,93 @@ with open('HW2/IMDB/IMDBPerson.txt', 'r',  encoding='latin-1') as file:
 
         try:
             cur.execute("INSERT INTO Person VALUES (%s, %s, %s, %s)",
-            (ID, fName, lName, gender))
+                (ID, fName, lName, gender))
             conn.commit()
         except:
             conn.rollback()
-            print("Error ID:"+ ID)
-            
-            
+            print("Error ID:"+ ID)        
+'''  
+'''
+# Add data to director table      
+with open('HW2/IMDB/IMDBDirectors.txt', 'r', encoding='latin-1') as file:
+    #skip the first line with file lables
+    file.readline()
 
+    for line in tqdm(file, total= 86880):
+        splitString = line.strip().split(',')
+        ID = splitString[0]
+        fName = splitString[1]
+        lName = splitString[2]
+
+        try:
+            cur.execute("INSERT INTO Director VALUES (%s, %s, %s)",
+                (ID, fName, lName))
+            conn.commit()
+        except:
+            conn.rollback()
+            print("Error ID:"+ ID) 
+'''
+'''
+# Add data to Movie table
+with open('HW2/IMDB/IMDBMovie.txt', 'r', encoding='latin-1') as file:
+    #skip the first line with file lables
+    file.readline()
+
+    for line in tqdm(file, total= 388269):
+        splitString = line.strip().split('),')
+        frontOfRow = splitString[0] + ')'
+        backOfRow = splitString[1].split(',')
+        frontOfRow = frontOfRow.split(',', 1)
+        ID = frontOfRow[0]
+        name = frontOfRow[1]
+        year = backOfRow[0]
+        rank = backOfRow[1] if backOfRow[1] !="" else None
+
+        try:
+            cur.execute("INSERT INTO Movie VALUES (%s, %s, %s, %s)",
+                (ID, name, year, rank))
+            conn.commit()
+        except:
+            conn.rollback()
+            print("Error ID:"+ ID) 
+'''  
+# Add data to ActsIn table
+with open('HW2/IMDB/IMDBCast.txt', 'r', encoding='latin-1') as file:
+    #skip the first line with file lables
+    file.readline()
+
+    for line in tqdm(file, total= 3432630):
+        splitString = line.split(',')
+        pid = splitString[0]
+        mid = splitString[1]
+        role = splitString[2] if splitString[2] !="" else None
+        if role != None:
+            role = role.replace('[','').replace(']','').strip()
+        try:
+            cur.execute("INSERT INTO actsin VALUES (%s, %s, %s)",
+                (pid, mid, role))
+            conn.commit()
+        except Exception as e:
+            conn.rollback()
+            print(e)
+
+# Add data to Directs table
+with open('HW2/IMDB/IMDBMovie_Directors.txt', 'r', encoding='latin-1') as file:
+    #skip the first line with file lables
+    file.readline()
+
+    for line in tqdm(file, total= 406967):
+        splitString = line.split(',')
+        pid = splitString[0]
+        mid = splitString[1]
+
+        try:
+            cur.execute("INSERT INTO directs VALUES (%s, %s)",
+                (pid, mid))
+            conn.commit()
+        except Exception as e:
+            conn.rollback()
+            print(e)   
 
 cur.close()
 conn.close()
